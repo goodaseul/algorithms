25f3e1a819b6414ef450b5d3f099ffca675cadd5
Jeong Daseul <98886223+goodaseul@users.noreply.github.com>
2026-01-20 21:08:20 +0900

    [level 0] Title: 배열 만들기 3, Time: 5.45 ms, Memory: 44.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181895. 배열 만들기 3/배열 만들기 3.js
@@ -0,0 +1,7 @@
+function solution(arr, intervals) {
+    var answer = [];
+    const intervalFirst = arr.slice(intervals[0][0], intervals[0][1] + 1);
+    const intervalSecond = arr.slice(intervals[1][0], intervals[1][1] + 1);
+    answer = [...intervalFirst, ...intervalSecond];
+    return answer;
+}
