711394eaa6405bbdb4d213c00e1eba8debc9dc13
Jeong Daseul <98886223+goodaseul@users.noreply.github.com>
2026-01-28 11:09:59 +0900

    [level 1] Title: 음양 더하기, Time: 0.15 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/76501. 음양 더하기/음양 더하기.js
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.js
@@ -1,6 +1,10 @@
+// function solution(absolutes, signs) {
+//     let answer = absolutes.map((ab, idx) => {
+//        return signs[idx] ? ab : -ab
+//     })
+//     return answer.reduce((acc, cur) => acc + cur, 0)
+// }
 function solution(absolutes, signs) {
-    let answer = absolutes.map((ab, idx) => {
-       return signs[idx] ? ab : -ab
-    })
-    return answer.reduce((acc, cur) => acc + cur, 0)
+    return absolutes.reduce((acc, cur, idx) => acc += signs[idx] ? cur: -cur 
+    , 0)
 }
