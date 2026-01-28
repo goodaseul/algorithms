f9ec1c4ef8de2836fde83ad3b13179763d6e492d
Jeong Daseul <98886223+goodaseul@users.noreply.github.com>
2026-01-28 12:14:01 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.14 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -0,0 +1,16 @@
+function solution(num) {
+    let count = 0;
+    
+    while( num !== 1 && count < 500) {
+        if( num % 2 === 0) {
+            num = num / 2
+            count++
+        }else {
+            num = num * 3 + 1
+            count++
+        }
+        
+    }
+    
+    return num === 1 ? count : -1;
+}
