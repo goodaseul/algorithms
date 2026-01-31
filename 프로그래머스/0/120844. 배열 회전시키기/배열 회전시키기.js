81df9ec31fcab64b1e94d0479a23f1bae6038edc
Jeong Daseul <98886223+goodaseul@users.noreply.github.com>
2026-01-31 10:27:22 +0900

    [level 0] Title: 배열 회전시키기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120844. 배열 회전시키기/배열 회전시키기.js
@@ -0,0 +1,6 @@
+function solution(numbers, direction) {
+    if( direction === "right") {
+        return [numbers[numbers.length - 1] , ...numbers.slice(0,-1)]
+    }
+    return [...numbers.slice(1), numbers[0] ]
+}
