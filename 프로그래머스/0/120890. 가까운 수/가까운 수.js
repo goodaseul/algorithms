7642321e73912d85992248f9e74e7edc7e01ca98
Jeong Daseul <98886223+goodaseul@users.noreply.github.com>
2026-02-04 09:54:25 +0900

    [level 0] Title: 가까운 수, Time: 0.10 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120890. 가까운 수/가까운 수.js
+++ 프로그래머스/0/120890. 가까운 수/가까운 수.js
@@ -1,14 +1,19 @@
 function solution(array, n) {
-    // 기준 그냥 처음꺼 잡기
-    let answer = array[0];
-    let minDiff = Math.abs(array[0] - n);
-    // 하나씩 비교
-    for( let i of array ){
-        const diff = Math.abs(i - n);
-        if( minDiff > diff || diff == minDiff && i < answer) {
-            answer = i
-            minDiff = diff
-        }
-    }
-    return answer;
+    const answer = [...array].sort((a,b) => Math.abs( n - a) - Math.abs( n - b) || a - b);
+    return answer[0]
 }
+
+// function solution(array, n) {
+//     // 기준 그냥 처음꺼 잡기
+//     let answer = array[0];
+//     let minDiff = Math.abs(array[0] - n);
+//     // 하나씩 비교
+//     for( let i of array ){
+//         const diff = Math.abs(i - n);
+//         if( minDiff > diff || diff == minDiff && i < answer) {
+//             answer = i
+//             minDiff = diff
+//         }
+//     }
+//     return answer;
+// }
