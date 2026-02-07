a8c93121a3e52958d209e3794dca7fa033b25636
Jeong Daseul <98886223+goodaseul@users.noreply.github.com>
2026-02-07 15:54:35 +0900

    [level 0] Title: k의 개수, Time: 21.18 ms, Memory: 45.6 MB -BaekjoonHub

--- 프로그래머스/0/120887. k의 개수/k의 개수.js
+++ 프로그래머스/0/120887. k의 개수/k의 개수.js
@@ -1,12 +1,3 @@
 function solution(i, j, k) {
-    let count = 0;
-    for( let n = i; n<=j; n ++){
-        let str = n.toString();
-        for( let char of str){
-            if( char === k.toString()){
-                count ++
-            }
-        }
-    }
-    return count
+    return Array(j-i+1).fill(i).map((value,index) => value + index).join("").split(k).length - 1
 }
