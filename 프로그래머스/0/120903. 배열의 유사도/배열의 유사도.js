1c4bcb3c9a078bde58c78059cbbf939337ef872c
Jeong Daseul <98886223+goodaseul@users.noreply.github.com>
2026-02-07 15:10:30 +0900

    [level 0] Title: 배열의 유사도, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.js
+++ 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.js
@@ -1,3 +1,5 @@
 function solution(s1, s2) {
-    return s1.filter( s => s2.includes(s)).length;
+    // return s1.filter( s => s2.includes(s)).length;
+    //교집합
+    return s1.length + s2.length - new Set([...s1, ...s2]).size
 }
