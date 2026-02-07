42cf09d6ef9ecee4566ec3cf0f302372027f9f77
Jeong Daseul <98886223+goodaseul@users.noreply.github.com>
2026-02-07 15:40:24 +0900

    [level 0] Title: A로 B 만들기, Time: 0.74 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120886. A로 B 만들기/A로 B 만들기.js
+++ 프로그래머스/0/120886. A로 B 만들기/A로 B 만들기.js
@@ -1,4 +1,3 @@
 function solution(before, after) {
-    const check = before.split("").sort().join("") === after.split("").sort().join("") ? 1 : 0
-    return check;
+    return before.split("").sort().join("") === after.split("").sort().join("") ? 1 : 0
 }
