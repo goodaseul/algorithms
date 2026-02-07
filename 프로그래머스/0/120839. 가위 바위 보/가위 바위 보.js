8a9c98e59863d610f947162c4f5bb9e31738245b
Jeong Daseul <98886223+goodaseul@users.noreply.github.com>
2026-02-07 15:21:41 +0900

    [level 0] Title: 가위 바위 보, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.js
+++ 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.js
@@ -1,10 +1,17 @@
 function solution(rsp) {
-    const newArr = rsp.split("");
-    const result = [];
-    newArr.map((rspNum) => {
-        rspNum === "2" && result.push("0");
-        rspNum === "0" && result.push("5");
-        rspNum === "5" && result.push("2");
-     })
-    return result.join().replaceAll(",","")
+    let answer = {
+        "0" : "5",
+        "2" : "0",
+        "5" : "2"
+    }
+    return [...rsp].map((value) => answer[value]).join("")
+    
+//     const newArr = rsp.split("");
+//     const result = [];
+//     newArr.map((rspNum) => {
+//         rspNum === "2" && result.push("0");
+//         rspNum === "0" && result.push("5");
+//         rspNum === "5" && result.push("2");
+//      })
+//     return result.join().replaceAll(",","")
 }
