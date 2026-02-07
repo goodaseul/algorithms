b5c88ad057db53a3564891b3c6dd9773a962cab1
Jeong Daseul <98886223+goodaseul@users.noreply.github.com>
2026-02-07 14:51:25 +0900

    [level 0] Title: 양꼬치, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120830. 양꼬치/양꼬치.js
+++ 프로그래머스/0/120830. 양꼬치/양꼬치.js
@@ -1,16 +1,6 @@
 function solution(n, k) {
-    const nPrice = 12000;
-    const kPrice = 2000;
-    
-    let price;
-    
-    const calc = Math.trunc(n / 10);
-    
-    if( (n % 10) + 1 ){
-        price = (n * nPrice) + (k - calc ) * kPrice;
-    } else {
-        price = (n * nPrice) + (k * kPrice)
+    if( n >= 10) {
+        k -= ~~(n/10)
     }
-    
-    return price
+    return (12000 * n) + (2000 * k);
 }
