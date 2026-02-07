97e670528556772b758c6b7480c92b832d2ad3dc
Jeong Daseul <98886223+goodaseul@users.noreply.github.com>
2026-02-07 14:55:17 +0900

    [level 0] Title: 짝수의 합, Time: 0.20 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120831. 짝수의 합/짝수의 합.js
+++ 프로그래머스/0/120831. 짝수의 합/짝수의 합.js
@@ -1,7 +1,7 @@
 function solution(n) {
-   let answer = 0;
-    for ( let i = 0; i <= n; i ++) {
-        if ( i % 2 === 0) answer += i;
-    }
-    return answer
+   // let answer = 0;
+    // for ( let i = 0; i <= n; i ++) {
+    //     if ( i % 2 === 0) answer += i;
+    // }
+    return Array(n).fill().map((_,index) => index+1).filter((value) => value % 2 === 0).reduce((acc,cur) => acc + cur, 0)
 }
