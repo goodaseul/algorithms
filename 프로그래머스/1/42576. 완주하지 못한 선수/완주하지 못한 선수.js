f13fa41ea5c0d945e178c12cc360938fe6f19564
Jeong Daseul <98886223+goodaseul@users.noreply.github.com>
2026-02-07 10:17:52 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 38.66 ms, Memory: 66.7 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,23 +1,25 @@
+// sort
 // function solution(participant, completion) {
-//     participant.sort();
-//     completion.sort();
-//     for( let i = 0; i < participant.length; i++){
-//         if( participant[i] !== completion[i]){
-//             return participant[i]
+//     const participantSort = [...participant].sort();
+//     const completionSort = [...completion].sort();
+//     for(let i = 0; i < completionSort.length; i ++){
+//         if( completionSort[i]  !== participantSort[i] ){
+//             return participantSort[i] 
 //         }
 //     }
-// }
-
+//     return participantSort[participantSort.length - 1]
+// }   
+//hash
 function solution(participant, completion) {
-    // 해시
     const map = new Map();
-    for(const name of participant){
+    for( const name of participant ) {
         map.set(name, (map.get(name) || 0) + 1)
     }
-    for (const name of completion){
-        map.set(name, map.get(name) - 1)
+    for( const name of completion ){
+        map.set(name, (map.get(name) - 1))
     }
-    for( const [name, count] of map){
-        if (count > 0) return name;
-    }
+    for( const [name, count] of map)  {
+        if( count > 0 ) return name;
+    } 
+    
 }
