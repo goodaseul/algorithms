4e880baaa37c8847492ccef3eec9f0dc57f6ba54
Jeong Daseul <98886223+goodaseul@users.noreply.github.com>
2026-02-23 20:04:35 +0900

    [level 1] Title: 평균 구하기, Time: 0.18 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12944. 평균 구하기/평균 구하기.js
+++ 프로그래머스/1/12944. 평균 구하기/평균 구하기.js
@@ -1,6 +1,7 @@
 function solution(arr) {
     let result = 0;
-    const newArr = [...arr].map( num => result += num);
-    result = result / newArr.length;
-    return result;
+    for( i of arr ){
+        result += i;
+    }
+    return result / arr.length;
 }
