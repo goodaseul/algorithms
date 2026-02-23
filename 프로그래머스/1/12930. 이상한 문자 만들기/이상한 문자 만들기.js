8d230a8159a7a6b7edaddba3548c06a13aa5e817
Jeong Daseul <98886223+goodaseul@users.noreply.github.com>
2026-02-23 20:48:23 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.25 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
@@ -1,14 +1,9 @@
 function solution(s) {
-  let count = 0;
-    let answer = [];
-    for( let char of s){
-        if( char === " "){
-            answer.push(" ");
-            count = 0;
-        }else {
-            answer.push( count % 2 === 0 ? char.toUpperCase() : char.toLowerCase() )
-            count ++
-        }
-    }
-    return answer.join("");
+    return s.split(" ")
+        .map(word => word.split("")
+             .map((char, idx) => idx % 2 === 0 ? 
+                  char.toUpperCase() : char.toLowerCase()
+        ).join("")
+    ).join(" ")
 }
+
