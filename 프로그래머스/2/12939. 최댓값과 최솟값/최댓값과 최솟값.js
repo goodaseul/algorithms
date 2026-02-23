b7d8cdecfd509eb8bb4fdefc1c46de1e7ebf03ed
Jeong Daseul <98886223+goodaseul@users.noreply.github.com>
2026-02-23 20:32:33 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
@@ -1,7 +1,4 @@
 function solution(s) {
-    const sortArr = s.split(" ").sort((a,b) => a - b).map(Number);
-    const max = Math.max(...sortArr);
-    const min = Math.min(...sortArr);
-    let num = `${min} ${max}` 
-    return num
+    const arr = s.split(" ");
+    return `${Math.min(...arr)} ${Math.max(...arr)}`
 }
