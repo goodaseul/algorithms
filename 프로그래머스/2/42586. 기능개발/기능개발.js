9f002bf237ca5a0e8c4fb608f86091f1966aba00
Jeong Daseul <98886223+goodaseul@users.noreply.github.com>
2026-02-26 17:43:09 +0900

    [level 2] Title: 기능개발, Time: 0.24 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.js
+++ 프로그래머스/2/42586. 기능개발/기능개발.js
@@ -1,20 +1,21 @@
 function solution(progresses, speeds) {
     const days = progresses.map((progress, index) => 
-         Math.ceil((100 - progress) / speeds[index])
-    )
-    const dayArr = [];
-    let current = days[0];
+        Math.ceil((100 - progress) / speeds[index])                           
+    );
+    const result = [];
+    let current = days[0]
     let count = 1;
-    for( let i = 1; i < days.length; i ++){
-        if( days[i] <= current ){
-            count ++;
-        }else { 
-            dayArr.push(count);
-            current = days[i]
-            count = 1;  
+    // count 2 
+    for(let i = 1; i < progresses.length; i++){
+        if( days[i] <= current) {
+            count++
+        }else{
+            result.push(count); // 2
+            current = days[i]; // 현재 바뀜
+            count = 1; // count 다시 1 
         }
     }
-    dayArr.push(count);
-    return dayArr
+    result.push(count); // 마지막은 비교군이 없음
+    return result;
 }
 
