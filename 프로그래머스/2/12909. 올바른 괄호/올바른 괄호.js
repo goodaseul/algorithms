0cb12fa739ebd8d1bc25c5ea74a3a6383c3e5a3b
Jeong Daseul <98886223+goodaseul@users.noreply.github.com>
2026-02-27 13:03:50 +0900

    [level 2] Title: 올바른 괄호, Time: 31.12 ms, Memory: 37.8 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
@@ -1,18 +1,14 @@
 function solution(s){
     let count = 0;
-    
-    for(char of s){
-        if( char === "("){
-            count ++
-        }else if ( char === ")"){
-            count --
+    for( const char of s){
+        if(char === "(") {
+            count ++;
+        }else if (char === ")"){
+            count --;
         }
-        
-        if( count < 0){
-            return false
+        if( count < 0) {
+            return false;
         }
     }
-    
- return   count === 0
-   
+    return count === 0;
 }
