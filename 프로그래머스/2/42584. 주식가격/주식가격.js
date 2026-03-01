444eb9d388173c9b66507bbba62508b61c2215e4
Jeong Daseul <98886223+goodaseul@users.noreply.github.com>
2026-03-01 16:16:17 +0900

    [level 2] Title: 주식가격, Time: 29.40 ms, Memory: 43.8 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.js
+++ 프로그래머스/2/42584. 주식가격/주식가격.js
@@ -1,15 +1,17 @@
 function solution(prices) {
     const stack = [];
     const answer = new Array(prices.length).fill(0);
-    console.log( answer );
     
-    for( let i = 0; i < prices.length; i++) {
-        while(stack.length && prices[i] < prices[stack[stack.length - 1]]){
-            // 지금 가격이 이전가격보다 작은가?
+    for( let i = 0; i< prices.length; i ++){
+        // [1,2,3,2,3] 2 < prices[0]
+        while(stack.length && prices[i] < prices[stack[stack.length - 1]] ){
+            // 현재값이 이전값보다 작다면? 
+            // console.log( stack) // 0,1,2 pop => 0,1
             const prevIndex = stack.pop();
             answer[prevIndex] = i - prevIndex;
         }
-        stack.push(i) // stack 는 인덱스가 쌓임
+        // 가격이 떨어지지 않은 인덱스
+        stack.push(i) // [0,1,2,3,4]
     }
     while(stack.length > 0) {
         const prevIndex = stack.pop();
@@ -17,13 +19,8 @@ function solution(prices) {
     }
     return answer 
 }
-
 // 스택 만든다
-
 // answer 배열 만든다
-
 // for 돌린다
-
 // while 조건 적어본다
-
 // pop하고 기간 계산 적어본다
