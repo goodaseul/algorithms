78564d72ecaa3a4c9f7aee6c82191d80d7f06f22
Jeong Daseul <98886223+goodaseul@users.noreply.github.com>
2026-03-01 18:54:45 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 28.34 ms, Memory: 37.9 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.js
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.js
@@ -1,24 +1,19 @@
 function solution(bridge_length, weight, truck_weights) { // 2 / 10 / [7,4,5,6]
-    let time = 0;  // 총 시간
-    let totalWeigth = 0; // 현재 다리 위 총 무게
-    let bridge = new Array(bridge_length).fill(0); // 다리 상태 (고정 길이)
-    while(truck_weights.length > 0 || totalWeigth > 0) {
+    let time = 0;
+    let totalWeight = 0;
+    let bridge = new Array(bridge_length).fill(0);
+    while( truck_weights.length > 0 || totalWeight > 0 ) {
+        // truck_weight 가 있고, 토탈 weight 가 weight 보다 작거나 같을때까지
         time++;
         const passedTruck = bridge.shift();
-        totalWeigth -= passedTruck;
-        // ex.0 - 7 => -7
-        if (
-          truck_weights.length > 0 &&
-          totalWeigth + truck_weights[0] <= weight
-        ) {
-          const newTruck = truck_weights.shift();
-          bridge.push(newTruck);
-          totalWeigth += newTruck;
-        } else {
-            // 대기 트럭이 없을 때, 
-            // 무게 초과라서 못 올릴 때 
-            // 결국 빈자리를 표현
-          bridge.push(0);
+        totalWeight -= passedTruck;
+        
+        if( truck_weights.length > 0  && totalWeight + truck_weights[0] <= weight) {
+            const newTruck = truck_weights.shift();
+            bridge.push(newTruck);
+            totalWeight += newTruck;
+        }else {
+            bridge.push(0)
         }
     }
     return time
