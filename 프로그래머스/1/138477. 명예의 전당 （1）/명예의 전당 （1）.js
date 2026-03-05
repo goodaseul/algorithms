794df02cfa3c11994de3fb3d5f2674e320f9ba8e
Jeong Daseul <98886223+goodaseul@users.noreply.github.com>
2026-03-05 17:11:02 +0900

    [level 1] Title: 명예의 전당 (1), Time: 22.16 ms, Memory: 36.2 MB -BaekjoonHub

--- 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.js
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.js
@@ -2,13 +2,13 @@ function solution(k, score) {
     let answer = [];
     let result = [];
     
-    for( let i = 0; i < score.length; i ++){
-        answer.push(score[i])
+    for(let i = 0; i < score.length; i ++) {
+        answer.push(score[i]);
         answer.sort((a,b) => b-a);
-        if( answer.length > k) {
-            answer.pop();
+        if(answer.length > k) {
+            answer.pop(); 
         }
         result.push(answer[answer.length - 1])
     }
-    return result
+    return result;
 }
