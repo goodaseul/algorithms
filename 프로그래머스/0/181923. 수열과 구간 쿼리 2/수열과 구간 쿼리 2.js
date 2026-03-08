9d700d673c55cc189724d3be555d5c4111e8d90b
Jeong Daseul <98886223+goodaseul@users.noreply.github.com>
2026-03-08 19:42:46 +0900

    [level 0] Title: 수열과 구간 쿼리 2, Time: 5.70 ms, Memory: 36.8 MB -BaekjoonHub

--- 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.js
+++ 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.js
@@ -1,12 +1,14 @@
 function solution(arr, queries) {
     let result = [];
-    queries.forEach(([s,e,k], i) => {
+    
+    queries.forEach(([s,e,k]) => {
         const filtered = arr.slice(s, e + 1).filter((value) => value > k);
-        if( filtered.length === 0) {
-            result.push(-1)
-        }else { 
-            result.push( Math.min(...filtered) )
+        if( filtered.length === 0 ){
+            result.push(-1);
+        }else {
+            result.push(Math.min(...filtered))
         }
     })
+    
     return result;
 }
