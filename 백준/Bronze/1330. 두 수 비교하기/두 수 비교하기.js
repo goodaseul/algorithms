72ebd7656bbbf51ade7b2aa48d629e6254fb2bbd
Jeong Daseul <98886223+goodaseul@users.noreply.github.com>
2026-03-09 20:05:05 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 92 ms, Memory: 9328 KB -BaekjoonHub

---
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+const [A, B] = fs.readFileSync(0, 'utf8').trim().split(' ').map(Number);
+if( A > B) {
+    console.log(">")
+}else if( A < B) {
+    console.log("<")
+}else {
+    console.log("==")
+}
