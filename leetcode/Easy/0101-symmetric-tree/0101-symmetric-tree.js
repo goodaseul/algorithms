1e37cea1dc697cc01470e5cba7847d5d6d11a1d1
Jeong Daseul <98886223+goodaseul@users.noreply.github.com>
2026-03-12 12:43:49 +0900

    Time: 0 ms (100%), Space: 55.7 MB (96.77%) - LeetHub

---
+++ leetcode/Easy/0101-symmetric-tree/0101-symmetric-tree.js
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+const isSymmetric = function(root) {
+    function treeNode(left, right) {
+        if( !left && !right) return true;
+        if( !left || !right) return false;
+
+        if( left.val !== right.val ) return false;
+
+        return (
+            treeNode(left.left, right.right) &&
+            treeNode(left.right, right.left)
+        )
+    }
+    return treeNode(root.left, root.right)
+};
