e014609e4c951f6269d8d094e806b7fc8629eea2
Jeong Daseul <98886223+goodaseul@users.noreply.github.com>
2026-03-13 14:15:13 +0900

    [level 0] Title: 조건에 맞게 수열 변환하기 2, Time: 660.44 ms, Memory: 126 MB -BaekjoonHub

---
+++ 프로그래머스/0/181881. 조건에 맞게 수열 변환하기 2/조건에 맞게 수열 변환하기 2.js
@@ -0,0 +1,15 @@
+function solution(arr) {
+    let count = 0;
+    while(true) {
+        const next = arr.map((num) => {
+              if (num >= 50 && num % 2 === 0) return num / 2;
+              if (num < 50 && num % 2 === 1) return num * 2 + 1;
+              return num;
+        })
+        if( arr.join("") === next.join("")) break;
+        
+        arr = next;
+        count++;
+    }
+    return count
+}
