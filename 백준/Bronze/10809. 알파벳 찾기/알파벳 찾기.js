b63fc066406aa1c52a8976948091cc8f6d2e4886
Jeong Daseul <98886223+goodaseul@users.noreply.github.com>
2026-03-17 20:58:34 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 104 ms, Memory: 9348 KB -BaekjoonHub

---
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+const input = fs.readFileSync(0, 'utf8').trim();
+
+let result = "";
+
+for( let i = 0; i < 26; i ++) {
+    const alphabet = String.fromCharCode(97 + i);
+    result += input.indexOf(alphabet) + " ";
+}
+console.log( result.trim() )
