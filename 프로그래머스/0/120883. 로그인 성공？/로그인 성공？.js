067b38336faba458c8a558634afc382b2ecb269c
Jeong Daseul <98886223+goodaseul@users.noreply.github.com>
2026-03-17 20:03:30 +0900

    [level 0] Title: 로그인 성공?, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120883. 로그인 성공？/로그인 성공？.js
+++ 프로그래머스/0/120883. 로그인 성공？/로그인 성공？.js
@@ -1,14 +1,5 @@
 function solution(id_pw, db) {
-    let message = "";
     const [id, pw] = id_pw;
-    for( let i = 0; i < db.length; i ++){
-        const [dbId, dbPw] = db[i];
-        
-        if (id === dbId) {
-            if (pw === dbPw) return "login";
-            return "wrong pw";
-        }
-    }
-
-    return "fail";
+    const map = new Map();
+    console.log(map.set(db) === id)
 }
