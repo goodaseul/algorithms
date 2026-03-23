7ea8f37d9f50ff6f1fa318e66a8350b1775557dc
Jeong Daseul <98886223+goodaseul@users.noreply.github.com>
2026-03-23 14:09:06 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- leetcode/Easy/0217-contains-duplicate/0217-contains-duplicate.js
+++ leetcode/Easy/0217-contains-duplicate/0217-contains-duplicate.js
@@ -4,10 +4,9 @@
  */
 var containsDuplicate = function(nums) {
     const set = new Set();
-    for(let num of nums) {
-        if( set.has(num)) return true;
-
-        set.add(num)
+    for(let i = 0; i < nums.length; i ++) {
+        if(set.has(nums[i])) return true
+        set.add(nums[i])
     }
     return false
 };
