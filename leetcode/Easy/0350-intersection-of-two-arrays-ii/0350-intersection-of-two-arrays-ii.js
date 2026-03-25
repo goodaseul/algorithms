7d747e79e641262e12c9c832a2731cc93abc6941
Jeong Daseul <98886223+goodaseul@users.noreply.github.com>
2026-03-25 14:30:40 +0900

    Time: 2 ms (59.01%), Space: 56.4 MB (31.79%) - LeetHub

---
+++ leetcode/Easy/0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var intersect = function(nums1, nums2) {
+    const map = new Map();
+    for( let num of nums1) {
+        map.set(num, (map.get(num) || 0) + 1)
+    }
+    let result= [];
+    for( let num of nums2){
+        if( map.get(num) > 0) {
+            result.push(num)
+            map.set(num, map.get(num) - 1)
+        }
+    }
+    return result
+};
