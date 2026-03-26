c5640af47a15893bc3df50fdc4f8fdc96379083a
Jeong Daseul <98886223+goodaseul@users.noreply.github.com>
2026-03-26 16:42:29 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- leetcode/Medium/0015-3sum/0015-3sum.js
+++ leetcode/Medium/0015-3sum/0015-3sum.js
@@ -1,29 +1,29 @@
-/**
- * @param {number[]} nums
- * @return {number[][]}
- */
- [-4,-1,-1,0,1,2]
 var threeSum = function(nums) {
-    nums.sort((a,b) => a -b);
-     const result = [];
-    for(let i = 0; i < nums.length - 2; i ++) {
-        if(i > 0 && nums[i] === nums[i -1]) continue;
+    nums.sort((a, b) => a - b);
+    let result = [];
 
-        let left = i+1;
+    for (let num = 0; num < nums.length - 2; num++) {
+
+        if (num > 0 && nums[num] === nums[num - 1]) continue;
+
+        let left = num + 1;
         let right = nums.length - 1;
 
-        while(left < right) {
-            const sum = nums[i] + nums[left] + nums[right];
-            if( sum === 0) {
-                result.push([nums[i],nums[left],nums[right]]);
-                while(nums[left] === nums[left + 1]) left++;
-                while(nums[right] === nums[right + 1]) right++;
-                left++
-                right--
-            }else if(sum > 0 ){
-                right--
-            }else {
-                left ++
+        while (left < right) {
+            const sum = nums[num] + nums[left] + nums[right];
+
+            if (sum === 0) {
+                result.push([nums[num], nums[left], nums[right]]);
+
+                while (left < right && nums[left] === nums[left + 1]) left++;
+                while (left < right && nums[right] === nums[right - 1]) right--;
+
+                left++;
+                right--;
+            } else if (sum > 0) {
+                right--;
+            } else {
+                left++;
             }
         }
     }
