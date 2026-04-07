4018cef095498f776e59f77a7fe0a68a8a1c9569
다슬 <98886223+goodaseul@users.noreply.github.com>
2026-04-07 17:49:54 +0900

    [level 1] Title: 카드 뭉치, Time: 0.12 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.js
@@ -0,0 +1,16 @@
+function solution(cards1, cards2, goal) {
+    let card1Pointer = 0;
+    let card2Pointer = 0;
+    let result = false;
+    for( let i = 0; i < goal.length; i++) {
+        if( cards1[card1Pointer] == goal[i]) {
+            card1Pointer++
+        }
+        else if(cards2[card2Pointer] == goal[i]){
+            card2Pointer++
+        }else {
+            return "No";
+        }
+    }
+    return "Yes";
+}
