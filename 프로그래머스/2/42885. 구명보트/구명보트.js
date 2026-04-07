cf72d070a6260770f4d7a39ed97058427212d1b7
다슬 <98886223+goodaseul@users.noreply.github.com>
2026-04-07 14:49:57 +0900

    [level 2] Title: 구명보트, Time: 13.89 ms, Memory: 38.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.js
@@ -0,0 +1,14 @@
+function solution(people, limit) {
+    let count = 0;
+    people.sort((a,b) => a - b);
+    let left = 0;
+    let right = people.length - 1
+    while(left <= right){
+        if(people[left] + people[right] <= limit) {
+            left++
+        }
+        right --;
+        count++
+    }
+    return count;
+}
