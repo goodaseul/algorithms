fbcfd333dc7eb488c5b264d110076da6d61b125d
다슬 <98886223+goodaseul@users.noreply.github.com>
2026-04-11 15:24:38 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.23 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.js
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.js
@@ -1,14 +1,12 @@
 function solution(n) {
     if(n <= 2) return n;
+    let dp = [];
+    dp[1]= 1;
+    dp[2] =2;
     
-    let first = 1;
-    let second = 2;
-    let result = 0;
-    
-    for( let i = 3; i <= n; i ++) { 
-        result = (first + second) % 1234567;
-        first = second;
-        second = result;
+    for(let i = 3; i <= n; i++) {
+        dp[i] = (dp[i - 1] + dp[i - 2]) % 1234567
     }
-    return result
+    
+    return dp[n]
 }
