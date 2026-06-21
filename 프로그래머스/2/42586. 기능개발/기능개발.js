63ad442e48dfc2d2eec65b8c1870600043741b68
다슬 <98886223+goodaseul@users.noreply.github.com>
2026-06-21 17:14:10 +0900

    [level 2] Title: 기능개발, Time: 0.12 ms, Memory: 43.7 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.js
+++ 프로그래머스/2/42586. 기능개발/기능개발.js
@@ -1,21 +1,21 @@
 function solution(progresses, speeds) {
     const days = progresses.map((progress, index) => 
-        Math.ceil((100 - progress) / speeds[index])                           
-    );
-    const result = [];
-    let current = days[0]
-    let count = 1;
-    // count 2 
-    for(let i = 1; i < progresses.length; i++){
+        Math.ceil((100 - progress) / speeds[index])
+    )
+    
+    const commitAtOnce = [];
+    let current = days[0];
+    let items = 1;
+    
+    for(let i = 1; i < progresses.length; i ++) {
         if( days[i] <= current) {
-            count++
-        }else{
-            result.push(count); // 2
-            current = days[i]; // 현재 바뀜
-            count = 1; // count 다시 1 
+            items++
+        }else {
+            commitAtOnce.push(items);
+            current = days[i];
+            items = 1;
         }
     }
-    result.push(count); // 마지막은 비교군이 없음
-    return result;
+    commitAtOnce.push(items)
+    return commitAtOnce
 }
-
