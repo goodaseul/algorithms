f5299eead5db926e0b55afedc064658481088c90
다슬 <98886223+goodaseul@users.noreply.github.com>
2026-06-21 17:27:58 +0900

    [level 2] Title: 올바른 괄호, Time: 21.54 ms, Memory: 50.5 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
@@ -1,14 +1,15 @@
 function solution(s){
     let count = 0;
-    for( const char of s){
-        if(char === "(") {
-            count ++;
-        }else if (char === ")"){
-            count --;
+    for(const char of s){
+        if(count < 0) {
+            return false
         }
-        if( count < 0) {
-            return false;
+        if( char === "(") {
+            count++
+        }else if( char === ")") {
+            count--
         }
     }
-    return count === 0;
+    const result = count ===  0 ? true : false
+    return result
 }
