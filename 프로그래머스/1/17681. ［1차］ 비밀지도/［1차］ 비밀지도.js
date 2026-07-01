ae74a295d68916ae89388cdfcf34b965b1f676bb
다슬 <98886223+goodaseul@users.noreply.github.com>
2026-07-02 08:59:51 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.13 ms, Memory: 44.2 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
@@ -1,9 +1,9 @@
 function solution(n, arr1, arr2) {
     let answer = [];
     for(let i = 0; i < n; i ++){
-        let binary = ( arr1[i] | arr2[i] ).toString(2).padStart(n, 0);
-        let line = binary.replace(/1/g,"#").replace(/0/g, " ");
+        let binary = (arr1[i] | arr2[i]).toString(2).padStart(n, "0");
+        let line = binary.replace(/0/g, " ").replace(/1/g, "#");
         answer.push(line)
     }
-    return answer
+    return answer;
 }
